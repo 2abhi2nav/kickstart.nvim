aa3b35c998c8b2a03f1d4be4fe1e9ccc26dc8143
abhinav <abhinavrenji@gmail.com>
2025-12-10 23:59:17 +0530

    Enabled autoformatting

--- init.lua
+++ init.lua
@@ -639,7 +639,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -651,11 +651,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        c = { 'clang-format' },
+        python = { 'isort' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
